f0c47a8ed7ad562990bbe8a401e779a23848b5ae
Fajle Rabbi Choyon <79522975+choyon-droid@users.noreply.github.com>
2024-11-07 21:35:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        items: 1
+    });
+});
